900c0a521cd5d1c25d431f3d5548d44fe9bb7b3a
Casterial <Logan_Shelton@hotmail.com>
2023-12-24 23:10:13 -0800

    Update class_picker.py
    Updating to modern 3.7 Python

--- class_picker.py
+++ class_picker.py
@@ -1,53 +1,50 @@
 import random
 
-allClasses = ['Warrior', 'Ranger', 'Sorceress', 'Berserker', 'Tamer', 'Musa', 'Maehwa',
+available_classes = ['Warrior', 'Ranger', 'Sorceress', 'Berserker', 'Tamer', 'Musa', 'Maehwa',
               'Valkyrie', 'Kunoichi', 'Ninja', 'Wizard', 'Witch', 'Mystic', 'Striker', 'Lahn',
-              'Archer', 'Dark Knight', 'Shai', 'Guardian', 'Hashashin', 'Nova', 'Sage', 'Corsair', 'Drakania', 'Woosa', 'Maegu']
+              'Archer', 'Dark Knight', 'Shai', 'Guardian', 'Hashashin', 'Nova', 'Sage', 'Corsair', 'Drakania', 'Woosa', 'Maegu',
+              'Scholar']
 
 
 awakeVSucc = ['Awakening', 'Succession']
 
 
-print('Class List: {}'.format(allClasses))
-print('\n\nPlease follow the prompt, remember its case sensitive...\n')
-resp = ''
-while resp != 'exit':
-    resp = input("Any class you wish to not randomly pick? exit to exit:\t")
-    if resp != 'exit':
-        try:
-            allClasses.remove(resp)
-        except ValueError:
-            print("\n\nYou may have mispelled that, please try again...\n")
-            
-print("Lets choose your class for the season....\n")
-def The_Random_Matcher(allClasses,awakeVSucc):
-    theClass = random.choice(allClasses)
+print("Black Desert Online Classes - 2023")
+for item in available_classes:
+    print(item)
+class_remove = input("\n\nWhich classes to exclude? (comma separated)\n").split(',')
 
-    # Logic because some classes are unique
-    if theClass not in ['Shai', 'Archer']:
+for bdo_class in class_remove:
+    # Making this not case sensitive for user input
+    class_lower = bdo_class.strip().lower()
+    available_classes = [value for value in available_classes if value.lower() != class_lower]
+       
+            
+def The_Random_Matcher(available_classes,awakeVSucc):
+    theClass = random.choice(available_classes)
+
+    # Three classes in the game are neither Awakening or Succession, they're Ascension.
+    if theClass not in ['Shai', 'Archer', 'Scholar']:
         theSpec = random.choice(awakeVSucc)
     else:
-        theSpec = 'Awakening'
-    # Not Released Yet
-    if theClass in ['Maegu', 'Woosa']:
-        theSpec = 'Sucession'
-        
-    print("Class: {}, Playstyle: {}".format(theClass, theSpec))
+        theSpec = 'Ascension'
 
-def main(allClasses,awakeVSucc):
+    print(f"\nRandomly Selected:\n \t• {theSpec} {theClass}")
+
+def main(available_classes,awakeVSucc):
     # Initial Selector
-    The_Random_Matcher(allClasses, awakeVSucc)
+    The_Random_Matcher(available_classes, awakeVSucc)
     defaultInput = ""
     # Trolling the indecisive people
     too_many_runs = int(0)
     # Main functional loop, re-select if not happy
     while defaultInput != 'n':
-        defaultInput = input("Would you like to run again? (y/n):\t")
+        defaultInput = input("\nWould you like to run again? (y/n): ")
         defaultInput.lower()
         if defaultInput == 'y':
             # Add too_many_runs to force break after 3 runs
             too_many_runs = too_many_runs + 1
-            The_Random_Matcher(allClasses, awakeVSucc)
+            The_Random_Matcher(available_classes, awakeVSucc)
         # This person really needs to pick a class already
         if too_many_runs == 3:
             print("\n\nYOU NEED TO CHOOSE! STOP RIGHT THERE CRIMINAL!")
@@ -55,4 +52,4 @@ def main(allClasses,awakeVSucc):
     exit_code = input("\n\n<Enter> to exit the application.")
 
 if __name__ == "__main__":
-    main(allClasses,awakeVSucc)
+    main(available_classes,awakeVSucc)
